16e0af50ecd4414a77bb087a2debe1420c2a0bd8
Erik Paulo Lacerda <erik.lacerda@gmail.com>
2015-01-20 10:53:48 -0200

    Conexão com banco heroku através de Spring.

--- src/main/java/com/softb/system/repository/config/RepositoryConfig.java
+++ src/main/java/com/softb/system/repository/config/RepositoryConfig.java
@@ -1,14 +1,19 @@
 package com.softb.system.repository.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.sql.DataSource;
 
 import liquibase.integration.spring.SpringLiquibase;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
@@ -19,6 +24,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class RepositoryConfig  {
 
     private final Logger logger = LoggerFactory.getLogger(RepositoryConfig.class);
+    @Autowired
+    private Environment environment;
     
     @Bean
     public SpringLiquibase liquibase(DataSource datasource) {
@@ -28,6 +35,11 @@ public class RepositoryConfig  {
         liquibase.setChangeLog("classpath:config/liquibase/master.xml");
         liquibase.setContexts("development, production");
         return liquibase;
+    }
+    
+    @Bean
+    public URI buildURIDatabase() throws URISyntaxException {
+    	return  new URI(environment.getProperty("DATABASE_URL"));
     }
 }
 
