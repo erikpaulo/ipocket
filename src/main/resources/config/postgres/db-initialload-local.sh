0e56c024979d4a4877823e51bf8fcc2fefca3589
Erik Lacerda <erik.lacerda@gmail.com>
2016-04-11 17:27:24 -0300

    Updating heroku-db export and local load data

--- src/main/resources/config/postgres/db-initialload-local.sh
+++ src/main/resources/config/postgres/db-initialload-local.sh
@@ -1,5 +1,13 @@
-TRUNCATE TABLE user_role CASCADE;
-TRUNCATE TABLE user_account CASCADE;
+#!/usr/bin/env bash
 
-copy "user_account" from '/Users/eriklacerda/Projects/ipocket/src/main/resources/config/postgres/load-data/db-postgres-load-1coin-user_account.csv' WITH (DELIMITER ',', NULL '');
-copy "user_role" from '/Users/eriklacerda/Projects/ipocket/src/main/resources/config/postgres/load-data/db-postgres-load-1coin-roles.csv' WITH (DELIMITER ',', NULL '');
+
+COPY USER_GROUP FROM '/Users/eriklacerda/Projects/ipocket/src/main/resources/config/postgres/load-data/db-postgres-load-1coin-group.csv' DELIMITER ',' CSV;
+COPY USER_ACCOUNT FROM '/Users/eriklacerda/Projects/ipocket/src/main/resources/config/postgres/load-data/db-postgres-load-1coin-user_account.csv' DELIMITER ',' CSV;
+COPY USER_ROLE FROM '/Users/eriklacerda/Projects/ipocket/src/main/resources/config/postgres/load-data/db-postgres-load-1coin-roles.csv' DELIMITER ',' CSV;
+copy CATEGORY from '/Users/eriklacerda/Projects/ipocket/heroku-db/CATEGORY.csv' WITH (DELIMITER ',', NULL '');
+copy SUBCATEGORY from '/Users/eriklacerda/Projects/ipocket/heroku-db/SUBCATEGORY.csv' WITH (DELIMITER ',', NULL '');
+copy ACCOUNT from '/Users/eriklacerda/Projects/ipocket/heroku-db/ACCOUNT.csv' WITH (DELIMITER ',', NULL '');
+copy ACCOUNT_ENTRY from '/Users/eriklacerda/Projects/ipocket/heroku-db/ACCOUNT_ENTRY.csv' WITH (DELIMITER ',', NULL '');
+copy BILL from '/Users/eriklacerda/Projects/ipocket/heroku-db/BILL.csv' WITH (DELIMITER ',', NULL '');
+copy BUDGET from '/Users/eriklacerda/Projects/ipocket/heroku-db/BUDGET.csv' WITH (DELIMITER ',', NULL '');
+copy BUDGET_ENTRY from '/Users/eriklacerda/Projects/ipocket/heroku-db/BUDGET_ENTRY.csv' WITH (DELIMITER ',', NULL '');
--- src/main/resources/config/postgres/db-postgres-clear.sql
+++ src/main/resources/config/postgres/db-postgres-clear.sql
@@ -4,13 +4,13 @@
 * heroku pg:psql --app ipocket < /Users/eriklacerda/Documents/Personal/Projetos/ipocket/src/main/resources/config/postgres/db-postgres-clear.sql
 */
 
-DROP TABLE IF EXISTS BILL_ENTRY;
+DROP TABLE IF EXISTS BUDGET;
+DROP TABLE IF EXISTS BUDGET_ENTRY;
 DROP TABLE IF EXISTS BILL;
 DROP TABLE IF EXISTS ACCOUNT_ENTRY;
 DROP TABLE IF EXISTS ACCOUNT;
+DROP TABLE IF EXISTS SUB_CATEGORY;
 DROP TABLE IF EXISTS CATEGORY;
-DROP TABLE IF EXISTS CATEGORY_GROUP;
 DROP TABLE IF EXISTS user_role;
 DROP TABLE IF EXISTS remember_me_token;
-DROP TABLE IF EXISTS user_social_connection;
 DROP TABLE IF EXISTS user_account;
