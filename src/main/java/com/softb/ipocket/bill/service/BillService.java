20dcc2542889029aff3c018c090d0cf4b9d74c79
Erik Paulo Lacerda <erik.lacera@gmail.com>
2017-01-29 16:14:36 -0200

    no message

--- src/main/java/com/softb/ipocket/bill/service/BillService.java
+++ src/main/java/com/softb/ipocket/bill/service/BillService.java
@@ -194,7 +194,7 @@ public class BillService {
      */
     public BudgetNodeRoot genBudget(Integer groupId) {
         Calendar today = Calendar.getInstance();
-        today.set(2016, Calendar.JANUARY, 1, 0, 0);
+        today.set(2016, Calendar.DECEMBER, 31, 23, 59);
         Calendar startYear = Calendar.getInstance();
         Calendar endYear = Calendar.getInstance();
         startYear.set( today.get( Calendar.YEAR ), Calendar.JANUARY, 1, 0, 0, 1 );
